16cbb00026326e40c645756f8d6373e21a517ce3
eja37 <eja37@njit.edu>
2024-11-20 12:10:38 -0500

    Q1-Q6

---
+++ lab09.js
@@ -0,0 +1,32 @@
+//Q1//
+let characters = [`Harry`, `Hermione`, `Draco`, `Luna`];
+for (let i = 0; i < characters.length; i++){
+    console.log(characters[i]);
+}
+
+//Q2//
+let firstName = "Harry";
+let house = "Gryffindor";
+
+console.log(`Welcome, ${firstName} of ${house}!`);
+
+
+//Q3//
+let characterName = "Hermione";
+
+console.log(characterName.toLowerCase());
+console.log(characterName.toUpperCase());
+
+//Q4//
+let spell = "Expelliarmus";
+
+console.log(spell.trim());
+
+//Q5//
+let quote = "I solemnly swear that I am up to no good";
+console.log(quote.slice(10,16));
+
+//Q6//
+let FirstName = "Ron"
+let LastName = "Weasley"
+console.log(FirstName.concat(` `, LastName));

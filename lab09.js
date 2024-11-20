ee23db45fc25f46adb069d75638dd117f714bbc1
eja37 <eja37@njit.edu>
2024-11-20 12:27:10 -0500

    Q7-Q9

--- lab09.js
+++ lab09.js
@@ -30,3 +30,23 @@ console.log(quote.slice(10,16));
 let FirstName = "Ron"
 let LastName = "Weasley"
 console.log(FirstName.concat(` `, LastName));
+
+//Q7//
+let sentence = 'Draco is a good wizard'
+console.log(sentence.replace(`good`,`bad`));
+
+//Q8//
+let houses = [`Gryffindor`, `Hufflepuff`, `Ravenclaw`]
+houses.push("Slytherin");
+console.log(houses)
+houses.pop()
+console.log(houses);
+
+//Q9//
+let spells = [`Alohomora`, `Lumos`, `Nox`];
+spells.unshift(`Accio`);
+console.log(spells)
+spells.shift();
+console.log(spells);
+
+//Q10//
